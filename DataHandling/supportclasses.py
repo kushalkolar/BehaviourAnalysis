3bbc585f6c673c800998e2544a74af5b777cd8e0
danieldondorp <danieldondorp@gmail.com>
2019-05-27 11:10:39 +0200

    bugfix 2 for issue #4.

--- DataHandling/supportclasses.py
+++ DataHandling/supportclasses.py
@@ -151,8 +151,13 @@ class DataHandler:
                             metadataframe[col + "_mean"] = [mean_turn(df[col].dropna().values)]
                             metadataframe[col + "_var"] = [var_turn(df[col].dropna().values)]
 
-                            metadataframe[col + "_mean_at_speed"] = [mean_turn(df[col][df["speed030"] > 500].dropna().values)]
-                            metadataframe[col + "_var_at_speed"] = [var_turn(df[col][df["speed030"] > 500].dropna().values)]
+                            try:
+                                metadataframe[col + "_mean_at_speed"] = [mean_turn(df[col][df["speed030"] > 500].dropna().values)]
+                                metadataframe[col + "_var_at_speed"] = [var_turn(df[col][df["speed030"] > 500].dropna().values)]
+                            except:
+                                metadataframe[col + "_mean_at_speed"] = [np.nan]
+                                metadataframe[col + "_var_at_speed"] = [np.nan]
+
 
                         elif col not in ["X","Y","X_zero","Y_zero","time","Frame", "stim_on"]:
                             metadataframe[col + "_mean"] = [df[col].mean()]
